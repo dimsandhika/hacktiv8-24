47af3c640a4ecc081e5725871b20a5e6a0caa640
Dimas <dimas@Macs-MacBook-Pro.local>
2018-09-16 15:55:07 +0700

    add exercise14.js

--- h8-p0-w4/exercise14(belum).js
+++ h8-p0-w4/exercise14(belum).js
@@ -1,40 +1,39 @@
 function sorting(arrNumber) {
-  var arrNumber = arrNumber.sort()
+  arrNumber = arrNumber.sort()
   return arrNumber
-}
-
-function getTotal(arrNumber) {
-  var tampung = 0
-  var angka = 0
-
-  for (var i = 0; i < arrNumber.length; i++) {
-    for (var j = 0; j < arrNumber.length; j++) {
-      if (arrNumber[i] === arrNumber[j] && i !== j) {
-        angka++
-      tampung = arrNumber[i]
-    }
-    if (angka > tampung) {
-      tampung = -1
-    }
-    }
   }
-  return arrNumber
-}
+  
+  function getTotal(arrNumber) {
+    var tampung = 0
+    var angka = 0
 
-function mostFrequentLargestNumbers(arrNumber) {
-  var listSort = sorting(arrNumber);
-  var countHighest = getTotal(listSort);
-  return countHighest;
-}
-
-console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
-//'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
-
-console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
-//'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
-
-console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
-//'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
-
-console.log(mostFrequentLargestNumbers([]));
- //''
+    for (var i = 0; i < arrNumber.length; i++) {
+      for (var j = 0; j < arrNumber.length; j++) {
+        if (arrNumber[i] === arrNumber[j]) {
+          angka++
+          tampung = arrNumber[i]
+        } else {
+          tampung = ''
+        }
+      }
+    }
+return tampung
+  }
+  
+  function mostFrequentLargestNumbers(arrNumber) {
+    var listSort = sorting(arrNumber);
+    var countHighest = getTotal(listSort);
+    return countHighest;
+  }
+  
+  console.log(mostFrequentLargestNumbers([2, 8, 4, 6, 8, 5, 8, 4]));
+  //'angka paling besar adalah 8 dan jumlah kemunculan sebanyak 3 kali'
+  
+  console.log(mostFrequentLargestNumbers([122, 122, 130, 100, 135, 100, 135, 150]));
+  //'angka paling besar adalah 150 dan jumlah kemunculan sebanyak 1 kali'
+  
+  console.log(mostFrequentLargestNumbers([1, 1, 1, 1]));
+  //'angka paling besar adalah 1 dan jumlah kemunculan sebanyak 4 kali'
+  
+  console.log(mostFrequentLargestNumbers([]));
+  //''
